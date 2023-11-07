550958f4e97fd8182030cba9868827a577db7bf1
Drunkiel <drunkielfel@gmail.com>
2023-11-07 17:23:53 +0100

    Update

--- Assets/Scripts/UI/BuildingUI.cs
+++ Assets/Scripts/UI/BuildingUI.cs
@@ -12,30 +12,45 @@ public class BuildingUI : MonoBehaviour
     public void UpdateData(BuildingID _buildingID)
     {
         buildingNameText.text = _buildingID.buildingName;
+        if(_buildingID.buildingSprites.Length == 0) Debug.LogError("Not enough sprites to show: " + gameObject.name);
 
-        switch (_buildingID.buildingSprites.Length)
+        switch (SetByBuildingSize(_buildingID.size))
         {
-            default:
-                Debug.LogError("Not enough sprites to show: " + gameObject.name);
-                break;
-
-            case 1:
+            case 0:
                 SetActive(0);
                 singleImage.sprite = _buildingID.buildingSprites[0];
                 break;
 
+            case 1:
+                SetActive(1);
+                for (int i = 0; i < 2; i++)
+                    doubleImage[i].sprite = _buildingID.buildingSprites[i];
+                break;
+
+            //!TODO change it to work on vertical
             case 2:
                 SetActive(1);
                 for (int i = 0; i < 2; i++)
                     doubleImage[i].sprite = _buildingID.buildingSprites[i];
                 break;
 
-            case 4:
+            case 3:
                 SetActive(2);
                 for (int i = 0; i < 4; i++)
                     quadroImage[i].sprite = _buildingID.buildingSprites[i];
                 break;
         }
+    }
+
+    private int SetByBuildingSize(Vector2 size)
+    {
+        if (size.x == 0 || size.y == 0) Debug.LogError("Building is too small: " + gameObject.name);
+        if (size.x == 1 && size.y == 1) return 0; //1:1
+        if (size.x == 2 && size.y == 1) return 1; //2:1
+        if (size.x == 1 && size.y == 2) return 2; //1:2
+        if (size.x == 2 && size.y == 2) return 3; //2:2
+
+        return 0;
     }
 
     private void SetActive(int i)
